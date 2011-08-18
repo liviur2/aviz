252b5b57eb31d03fd116a46f1bafb0572816677a
Liviu.r2 <liviu.r2@gmail.com>
2011-08-18 19:19:47 +0300

    commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avize::Application.config.secret_token = 'f0566f799f5d862801bda4860372777b50fc73b70a0260d77ab969c12d2e5104bbff59a5c44cc3ea6c9f3c57f160c284b23bb225925b8131631d10bce15f94f8'
